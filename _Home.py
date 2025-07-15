eaf8908ff81fe15242229df08dbf24d0364b28c7
MijdeMijs <midasstorm@gmail.com>
2025-07-16 01:44:27 +0200

    Test auth login

--- _Home.py
+++ _Home.py
@@ -1,6 +1,35 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 import pandas as pd
+
+# Authentication
+
+# Load your YAML config
+with open('../auth_config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# Create the authenticator object
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+# Render the login widget
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
 
 if st.button("Press"):
     st.write("Hello world")
