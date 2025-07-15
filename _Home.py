4686962cc5506655639a6d8ee7997a679747580d
MijdeMijs <midasstorm@gmail.com>
2025-07-16 00:04:45 +0200

    Initialize Thursday Filmday v2.0

---
+++ _Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import pandas as pd
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
