32289088a430955c71c01e801087eb2f7342a8c2
MijdeMijs <midasstorm@gmail.com>
2025-07-16 12:13:17 +0200

    Auth secrets debug 1

--- _Home.py
+++ _Home.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 # Authentication
 # Convert st.secrets["auth_config"] to a *real dict recursively*
+# Recursively convert st.secrets to a plain dict
 def convert_secrets_to_dict(secrets_obj):
     if isinstance(secrets_obj, st.runtime.secrets.Secrets):
         return {k: convert_secrets_to_dict(v) for k, v in secrets_obj.items()}
@@ -14,40 +15,44 @@ def convert_secrets_to_dict(secrets_obj):
     else:
         return secrets_obj
 
+# Use the converter
 config = convert_secrets_to_dict(st.secrets["auth_config"])
 
+# Debugging: Check the structure of the config
+st.write("Type of config:", type(config))
+st.write("Type of credentials:", type(config.get("credentials")))
+st.write("Type of usernames:", type(config["credentials"].get("usernames")))
+st.write("Complete config structure:", config)
+
 # Create the authenticator object
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
-)
+# authenticator = stauth.Authenticate(
+#     config['credentials'],
+#     config['cookie']['name'],
+#     config['cookie']['key'],
+#     config['cookie']['expiry_days']
+# )
 
 # Render the login widget
 # name, authentication_status, username = authenticator.login()
 
-try:
-    authenticator.login()
-except Exception as e:
-    st.error(e)
+# try:
+#     authenticator.login()
+# except Exception as e:
+#     st.error(e)
 
-auth_status = st.session_state.get('authentication_status')
+# auth_status = st.session_state.get('authentication_status')
 
-if auth_status is False:
-    st.error('Username/password is incorrect')
-    st.stop()
-elif auth_status is None:
-    st.warning('Please enter your username and password')
-    st.stop()
+# if auth_status is False:
+#     st.error('Username/password is incorrect')
+#     st.stop()
+# elif auth_status is None:
+#     st.warning('Please enter your username and password')
+#     st.stop()
 
 # If we reach here, the user is authenticated
-authenticator.logout()
-st.write(f'Welcome *{st.session_state.get("name")}*')
-st.title('Some content')
+# authenticator.logout()
+# st.write(f'Welcome *{st.session_state.get("name")}*')
+# st.title('Some content')
 
 # The rest of your app goes here
-st.write("Protected app content goes here.")
-
-if st.button("Press"):
-    st.write("Hello world")
+# st.write("Protected app content goes here.")
