2e8f78ca04c1c340b35194b66e763da1cfb0f94a
MijdeMijs <midasstorm@gmail.com>
2025-07-16 02:05:15 +0200

    Auth update 2 _Home.py

--- _Home.py
+++ _Home.py
@@ -19,7 +19,7 @@ authenticator = stauth.Authenticate(
 )
 
 # Render the login widget
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login("main")
 
 if authentication_status:
     authenticator.logout('Logout', 'main')
